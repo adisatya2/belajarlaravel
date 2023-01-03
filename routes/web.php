6c34fa06691a398a8b27de38f562594ee6e20223
adisatya2 <adisatya.d@gmail.com>
2023-01-03 19:31:58 +0700

    tes 1

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome', ['title' => 'Belajar Laravel - MAD']);
+    return view('welcome', ['title' => 'Belajar Laravel dan Git - Takanome']);
 });
 
 Route::get('/greeting', function () {
